e6a4e3f0f351e7af59a7da2f60cd7ef7b3f7d7eb
Shambonik <shambonik@gmail.com>
2019-11-13 15:04:26 +0300

    base

---
+++ pyathashki/pyathashki/pyathashki.cpp
@@ -0,0 +1,163 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	while (true) {
+		setlocale(0, "");
+		short n;
+		cout << "Введите n (ширина и длина): ";
+		cin >> n;
+		int** array = new int* [n];
+		int** game = new int* [n];
+		int r = 1;
+		for (short i = 0; i < n; i++) {
+			array[i] = new int[n];
+			game[i] = new int[n];
+			for (short j = 0; j < n; j++) {
+				array[i][j] = i * n + j;
+				game[i][j] = i * n + j;
+			}
+		}
+
+		short i0 = 0;
+		short j0 = 0;
+		short i = 0;
+		short j = 0;
+		bool sw = true;
+		while (sw) {
+			for (int l = 0; l < n * n; l++) {
+				bool sw2 = true;
+				while (sw2) {
+					i = i0;
+					j = j0;
+					short z = rand() % 4;
+					switch (z) {
+					case 0:
+						i = i0 - 1;
+						sw = false;
+						break;
+					case 1:
+						i = i0 + 1;
+						sw = false;
+						break;
+					case 2:
+						j = j0 - 1;
+						sw = false;
+						break;
+					case 3:
+						j = j0 + 1;
+						sw = false;
+						break;
+					}
+					if ((i >= 0) && (j >= 0) && (i < n) && (j < n)) {
+						sw2 = false;
+					}
+				}
+				short x = game[i][j];
+				game[i][j] = game[i0][j0];
+				game[i0][j0] = x;
+				i0 = i;
+				j0 = j;
+			}
+			sw = false;
+			for (short i = 0; i < n; i++) {
+				for (short j = 0; j < n; j++) {
+					if (game[i][j] == array[i][j]) {
+						sw = true;
+					}
+				}
+			}
+		}
+
+		bool result = true;
+		cout << "Для конца игры пустая ячейка должна стоять в левом верхнем углу!\n";
+		cout << "0 - пустая ячейка\n";
+		while (result) {
+			for (short i = 0; i < n; i++) {
+				for (short j = 0; j < n; j++) {
+					cout << game[i][j];
+					if (game[i][j] > 99) {
+						cout << " ";
+					}
+					else if (game[i][j] > 9) {
+						cout << "  ";
+					}
+					else {
+						cout << "   ";
+					}
+				}
+				cout << endl;
+			}
+
+			sw = true;
+			while (sw) {
+				cout << "Введите строку и столбец ненулевой ячейки через пробел (нумераци с 1) : ";
+				cin >> i0 >> j0;
+				i0--;
+				j0--;
+				if ((i0 >= 0) && (j0 >= 0) && (i0 < n) && (j0 < n) && (game[i0][j0] != 0)) {
+					sw = false;
+				}
+				else {
+					cout << "Неверный ввод\n";
+				}
+			}
+			cout << "Выбранное число: " << game[i0][j0] << endl;
+			i = i0;
+			j = j0;
+			sw = true;
+			bool izm = false;
+			while (sw) {
+				cout << "Действие:\n1-наверх\n2-вниз\n3-влево\n4-вправо\n5-изменить ячейку\n";
+				short z = 0;
+				cin >> z;
+				switch (z) {
+				case 1:
+					i = i0 - 1;
+					sw = false;
+					break;
+				case 2:
+					i = i0 + 1;
+					sw = false;
+					break;
+				case 3:
+					j = j0 - 1;
+					sw = false;
+					break;
+				case 4:
+					j = j0 + 1;
+					sw = false;
+					break;
+				case 5:
+					izm = true;
+					sw = false;
+					break;
+				default:
+					cout << "Неверный ввод\n";
+					break;
+				}
+			}
+			if (!izm) {
+				if ((i >= 0) && (j >= 0) && (i < n) && (j < n) && (game[i][j] == 0)) {
+					game[i][j] = game[i0][j0];
+					game[i0][j0] = 0;
+					result = false;
+					for (short i = 0; i < n; i++) {
+						for (short j = 0; j < n; j++) {
+							if (game[i][j] != array[i][j]) {
+								result = true;
+							}
+						}
+					}
+				}
+				else {
+					cout << "Действие невозможно\n";
+				}
+			}
+		}
+		cout << "Победа!\n";
+	}
+
+}
---
+++ sapper/sapper/sapper.cpp
@@ -0,0 +1,262 @@
+﻿#include <iostream>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+int n = 9;
+short cursorPositionOld[2] = { 0,0 };
+short cursorPosition[2] = { 0,0 };
+int bombs[9][9];
+bool opened[9][9];
+bool flagged[9][9];
+int status = 0;
+int n_bombs = 12;
+COORD position;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+int minimum(int position) {
+	int m = 0;
+	if (position == 0) {
+		m = 0;
+	}
+	else {
+		m = position - 1;
+	}
+	return m;
+}
+
+int maximum(int position) {
+	int m = 0;
+	if (position == n - 1) {
+		m = n - 1;
+	}
+	else {
+		m = position + 1;
+	}
+	return m;
+}
+
+void clearBorder(int x, int y) {
+	bool left = false;
+	bool right = false;
+	bool up = false;
+	bool down = false;
+	if ((minimum(x) != x) && (opened[x - 1][y])) {
+		left = true;
+	}
+	if ((maximum(x) != x) && (opened[x + 1][y])) {
+		right = true;
+	}
+	if ((minimum(y) != y) && (opened[x][y - 1])) {
+		up = true;
+	}
+	if ((maximum(y) != y) && (opened[x][y + 1])) {
+		down = true;
+	}
+	for (int i = 0; i < 4; i++) {
+		if (i % 2 == 0) {
+			if (left) {
+				position = { (short)x * 4, (short)(y * 3 + (i/2)+1) };
+				SetConsoleCursorPosition(hConsole, position);
+				cout << " ";
+			}
+			if (right) {
+				position = { (short)(x+1) * 4,(short)(y * 3 + (i / 2)+1) };
+				SetConsoleCursorPosition(hConsole, position);
+				cout << " ";
+			}
+		}
+		if (up) {
+			position = { (short)(x * 4 + i),(short)y * 3};
+			SetConsoleCursorPosition(hConsole, position);
+			cout << " ";
+		}
+		if (down) {
+			position = { (short) (x * 4 + i),(short)(y+1) * 3};
+			SetConsoleCursorPosition(hConsole, position);
+			cout << " ";
+		}
+	}
+	SetConsoleCursorPosition(hConsole, { 2, 34 });
+}
+
+void flag() {
+	if (!opened[cursorPosition[0]][cursorPosition[1]]) {
+		position = { cursorPosition[0] * 4 + 1, cursorPosition[1] * 3 + 1 };
+		SetConsoleCursorPosition(hConsole, position);
+		if (flagged[cursorPosition[0]][cursorPosition[1]]) {
+			flagged[cursorPosition[0]][cursorPosition[1]] = false;
+			cout << " ";
+		}
+		else {
+			SetConsoleTextAttribute(hConsole, 14); //Цвет текста - желтый
+			flagged[cursorPosition[0]][cursorPosition[1]] = true;
+			cout << "?";
+			SetConsoleTextAttribute(hConsole, 15); //Цвет текста - белый
+		}
+	}
+	SetConsoleCursorPosition(hConsole, { 2, 34 });
+}
+
+void open(int x, int y) {
+	if (!opened[x][y]) {
+		position = {(short) x * 4 + 1, (short) y * 3 + 1 };
+		SetConsoleCursorPosition(hConsole, position);
+		opened[x][y] = true;
+		if (bombs[x][y] == -1) {
+			SetConsoleTextAttribute(hConsole, 12); //Цвет текста - свтело-красный
+			cout << "*";
+			status = -1;
+			SetConsoleCursorPosition(hConsole, { 2, 32 });
+			cout << "Поражение :(";
+		}
+		else {
+			if(bombs[x][y]!=0) cout << bombs[x][y];
+			clearBorder(x, y);
+		}
+		if (bombs[x][y] == 0) {
+			for (int i = minimum(y); i <= maximum(y); i++) {
+				for (int j = minimum(x); j <= maximum(x); j++) {
+					open(j, i);
+				}
+			}
+		}
+	}
+	SetConsoleCursorPosition(hConsole, { 2, 34 });
+}
+
+void generate() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			bombs[i][j] = 0;
+			opened[i][j] = false;
+			flagged[i][j] = false;
+		}
+	}
+	for (int i = 0; i < n_bombs; i++) {
+		int x = rand() % n;
+		int y = rand() % n;
+		while ((x >= minimum(cursorPosition[0])) && (x <= maximum(cursorPosition[0])) && (y >= minimum(cursorPosition[1])) && (y <= maximum(cursorPosition[1]))) {
+			x = (x + 4) % n;
+			y = (y - 4) % n;
+		}
+		while (bombs[x][y] == -1) {
+			x = (x - 6) % n;
+			y = (y + 3) % n;
+		}
+		bombs[x][y] = -1;
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (bombs[i][j] != -1) {
+				for (int k = minimum(i); k <= maximum(i); k++) {
+					for (int l = minimum(j); l <= maximum(j); l++) {
+						if (bombs[k][l] == -1)bombs[i][j]++;
+					}
+				}
+			}
+		}
+	}
+}
+
+void drawCursor() {
+	position = { cursorPositionOld[0] * 4 + 3, cursorPositionOld[1] * 3 + 1 };
+	SetConsoleCursorPosition(hConsole, position);
+	cout << " ";
+	position = { cursorPositionOld[0] * 4 + 3, cursorPositionOld[1] * 3 + 2 };
+	SetConsoleCursorPosition(hConsole, position);
+	cout << " ";
+	position = { cursorPositionOld[0] * 4 + 2, cursorPositionOld[1] * 3 + 2 };
+	SetConsoleCursorPosition(hConsole, position);
+	cout << " ";
+	position = { cursorPosition[0] * 4 + 3, cursorPosition[1] * 3 + 1 };
+	SetConsoleCursorPosition(hConsole, position);
+	cout << "|";
+	position = { cursorPosition[0] * 4 + 3, cursorPosition[1] * 3 + 2 };
+	SetConsoleCursorPosition(hConsole, position);
+	cout << "|";
+	position = { cursorPosition[0] * 4 + 2, cursorPosition[1] * 3 + 2 };
+	SetConsoleCursorPosition(hConsole, position);
+	cout << "_";
+	SetConsoleCursorPosition(hConsole, { 2, 34 });
+}
+
+void drawField() {
+	for (int i = 0; i <= n * 3; i++) {
+		for (int j = 0; j <= n * 4; j++) {
+			if (i % 3 == 0) {
+				cout << "-";
+			}
+			else if (j % 4 == 0) {
+				cout << "|";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+	cout <<"Перемещение курсора <- ->; Поставить флаг \"NUMPAD5\"; Рискнуть \"SPACE\"";
+}
+
+bool checkWin() {
+	bool result = true;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			result = ((bombs[i][j] == -1) && (flagged[i][j]))||((bombs[i][j] != -1)&&(opened[i][j]));
+			if (!result) break;
+		}
+		if (!result) break;
+	}
+	return result;
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	srand(time(nullptr));
+	SetConsoleTextAttribute(hConsole, 15); //Цвет текста - белый
+	drawField();
+	drawCursor();
+	int gen = 0;
+	while (status == 0) {
+		cursorPositionOld[0] = cursorPosition[0];
+		cursorPositionOld[1] = cursorPosition[1]; 
+
+		if (GetAsyncKeyState(VK_SPACE)) {
+			if (gen == 0) {
+				gen++;
+				generate();
+			}
+			open(cursorPosition[0], cursorPosition[1]);
+		}
+		else if (GetAsyncKeyState(VK_LEFT)) {
+			cursorPosition[0] = (cursorPosition[0] + n-1) % n;
+			drawCursor();
+		}
+		else if (GetAsyncKeyState(VK_RIGHT)) {
+			cursorPosition[0] = (cursorPosition[0] + 1) % n;
+			drawCursor();
+		}
+		else if (GetAsyncKeyState(VK_UP)) {
+			cursorPosition[1] = (cursorPosition[1] + n-1) % n;
+			drawCursor();
+		}
+		else if (GetAsyncKeyState(VK_DOWN)) {
+			cursorPosition[1] = (cursorPosition[1] + 1) % n;
+			drawCursor();
+		}
+		else if ((GetAsyncKeyState(VK_NUMPAD5))&&(gen!=0)){
+			flag();
+		}
+		Sleep(110);
+		if (checkWin()) {
+			SetConsoleTextAttribute(hConsole, 10); //Цвет текста - светло-зеленый
+			status = 1;
+			cout << "Победа!!! :)" << endl;
+		}
+	}
+	system("pause");
+}
+
